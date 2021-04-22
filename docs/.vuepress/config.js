b6feb0a8a0de8f204fe4e8ef57a7dc5a4a1d4f73
zh-liang <1185690533@qq.com>
2021-04-22 18:21:00 +0800

    alearn学习平台开发

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,12 @@
+const moment = require('moment-timezone');
+
 module.exports = {
     title: "alearn",
     description: "一个Java方向的编程知识整理平台",
+    head: [
+        ['meta', {name: 'keywords', content: 'alearn,aliang,java,springboot,springcloud,编程,知识整理'}]
+    ],
+    base: "/",
     markdown: {
         lineNumbers: true,
     },
@@ -15,18 +21,18 @@ module.exports = {
                 text: "线程",
                 link: "/thread/",
             },
-            {
-                text: "中间件",
-                link: "/middleware/",
-            },
+            // {
+            //     text: "中间件",
+            //     link: "/middleware/",
+            // },
             // {
             //     text: "SpringBoot",
             //     link: "/springboot/",
             // },
-            {
-                text: "SpringCloud",
-                link: "/springcloud/",
-            },
+            // {
+            //     text: "SpringCloud",
+            //     link: "/springcloud/",
+            // },
             {
                 text: "个人博客",
                 link: "https://www.aliang.link/",
@@ -55,7 +61,7 @@ module.exports = {
                 },
             ],
         },
-        lastUpdated: "上次编辑于",
+        lastUpdated: "上次编辑于 ",
         smoothScroll: true,
         repo: "https://github.com/imaliang/alearn",
         repoLabel: "查看源码",
@@ -63,13 +69,32 @@ module.exports = {
         docsBranch: "master",
         editLinks: true,
         editLinkText: "在 GitHub 上编辑此页",
+        locales: {
+            // 键名是该语言所属的子路径
+            // 作为特例，默认语言可以使用 '/' 作为其路径。
+            '/': {
+                lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+                title: 'alearn',
+                description: '一个Java方向的编程知识整理平台'
+            }
+        }
     },
     plugins: [
         "@vuepress/back-to-top",
+        "@vuepress/nprogress",
         ["@vuepress/medium-zoom", true],
         "@vuepress/active-header-links",
-        "@vuepress/last-updated",
         "@vuepress/plugin-nprogress",
+        ["@mr-hope/copy-code", {
+            selector: 'div[class*="language-"] pre',
+            duration: 2000,
+            showInMobile: false
+        }],
+        ["@vuepress/last-updated", {
+            transformer: (timestamp, lang) => {
+                return moment(timestamp).tz('Asia/Shanghai').locale(lang).format('yyyy-MM-DD HH:mm:ss')
+            }
+        }]
     ],
     extendMarkdown: (md) => {
         md.use(require("markdown-it-mark"));
--- docs/java/README.md
+++ docs/java/README.md
@@ -40,3 +40,36 @@ alearn 是一个Java方向的编程知识整理平台，便于自己查询与学
         merchantService.update(merchant);
     }
 ```
+
+```java
+    public void addLoginHistory(Integer merchantId, HttpServletRequest request) {
+        String ip = IPUtils.getIp(request);
+        LoginHistory loginHistory = new LoginHistory();
+        loginHistory.setLoginIp(ip);
+        loginHistory.setLoginCity("未知");
+        loginHistory.setMerchantId(merchantId);
+        loginHistory.setLoginTime(new Date());
+        String system = "其它";
+        String agent = request.getHeader("user-agent");
+        if (agent.contains("Windows")) {
+            system = "Windows客户端";
+        } else if (agent.contains("Mac")) {
+            system = "Mac客户端";
+        } else if (agent.contains("Android")) {
+            system = "Android移动客户端";
+        } else if (agent.contains("iPad")) {
+            system = "iPad客户端";
+        } else if (agent.contains("iPhone")) {
+            system = "iPhone移动客户端";
+        }
+        loginHistory.setLoginSystem(system);
+        //增加登录记录
+        loginHistoryService.insert(loginHistory);
+
+        // 更新商户的最后一次登录时间
+        Merchant merchant = new Merchant();
+        merchant.setId(merchantId);
+        merchant.setLoginTime(DateUtils.getNowDate());
+        merchantService.update(merchant);
+    }
+```
